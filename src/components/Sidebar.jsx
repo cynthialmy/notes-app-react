87a855f9f91f4eb411c578f5b99833216f23076f
Mengyuan Li <mli114@volvocars.com>
2023-11-10 22:54:12 +0100

    added notes app layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 export default function Sidebar(props) {
-	const noteElements = props.notes.map((note, index) => (
-		<div key={note.id}>
-			<div
-				className={`title ${
-					note.id === props.currentNote.id ? "selected-note" : ""
-				}`}
-				onClick={() => props.setCurrentNoteId(note.id)}
-			>
-				<h4 className="text-snippet">Note {index + 1}</h4>
+	const noteElements = props.notes.map((note, index) => {
+		const title = note.body.split("\n")[0].replace(/^#+/, "");
+		return (
+			<div key={note.id}>
+				<div
+					className={`title ${
+						note.id === props.currentNote.id ? "selected-note" : ""
+					}`}
+					onClick={() => props.setCurrentNoteId(note.id)}
+				>
+					<h4 className="text-snippet">{title}</h4>
+				</div>
 			</div>
-		</div>
-	));
+		);
+	});
 
 	return (
 		<section className="pane sidebar">
